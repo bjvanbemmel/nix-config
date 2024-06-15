631d1eff44095a7c5a92a9e691d12f2c3433eac0
bjvanbemmel <beauvanbemmel@live.nl>
2024-06-16 00:59:40 +0200

    configured zsh and updated flake

--- user/shell/zsh/zsh.nix
+++ user/shell/zsh/zsh.nix
@@ -1,11 +1,31 @@
 { config, pkgs, ... }:
 
 {
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
   programs.zsh = {
     enable = true;
     shellAliases = {
       v = "nvim";
     };
+
+    autosuggestion.enable =  true;
+    syntaxHighlighting.enable = true;
+
+    plugins = [
+      {
+        name = "pure";
+        src = pkgs.fetchFromGitHub {
+          owner = "sindresorhus";
+          repo = "pure";
+          rev = "v1.23.0";
+          hash = "sha256-Hdb5wGVkNrmmVWZaKf3xUnNYsTX/8Bb7AhgLNJxNAUc=";
+        };
+      }
+    ];
   };
 }
 	
