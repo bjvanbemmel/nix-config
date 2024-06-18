b1733015ee5391779cfc86ca209572a0bdafdfba
bjvanbemmel <beauvanbemmel@live.nl>
2024-06-18 19:35:57 +0200

    chromium now tagets the ungoogled-chromium package

--- user/app/chromium/chromium.nix
+++ user/app/chromium/chromium.nix
@@ -3,6 +3,7 @@
 {
   programs.chromium = {
     enable = true;
+    package = pkgs.ungoogled-chromium;
     extensions = [
       "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
       "lkhiljgmbeecmljiogckofcalncmfnfo" # Migaku
