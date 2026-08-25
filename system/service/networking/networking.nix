be42acec9d64599aa1c659aba658386efbb5e203
bjvanbemmel <beauvanbemmel@live.nl>
2026-08-25 12:40:08 +0200

    add vault.local to hosts

--- system/service/networking/networking.nix
+++ system/service/networking/networking.nix
@@ -5,6 +5,7 @@
     hosts = {
       "10.0.0.1" = [ "bjvanbemmel.local" ];
       "10.0.0.3" = [ "storage.bjvanbemmel.local" ];
+      "10.139.57.1" = [ "vault.local" ];
     };
     networkmanager.enable = true;
   };
