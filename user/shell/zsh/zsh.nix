40de86607722fa9dab4585aa4a4eb303e0cccaba
bjvanbemmel <beauvanbemmel@live.nl>
2026-08-26 01:37:59 +0200

    add "wakey" alias to perform WoL

--- user/shell/zsh/zsh.nix
+++ user/shell/zsh/zsh.nix
@@ -11,6 +11,7 @@
     shellAliases = {
       v = "nvim";
       nd = "nix develop -c $SHELL";
+      wakey = "ssh beauv@192.168.2.54 18:C0:4D:61:94:49";
     };
 
     autosuggestion.enable =  true;
